46cd6cc0574d55646c6e8596649b339c5ce4b17d
Siliang Chang <siliang.chang@gmail.com>
2020-02-06 22:17:34 -0700

    s10: spring config with Java code

---
+++ spring-demo-annotations/src/com/siliangchang/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.siliangchang.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.siliangchang.springdemo")
+public class SportConfig {
+	
+}
