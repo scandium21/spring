e160b2054fcc53ac837d5acb9ee4575a5131d4ad
Siliang Chang <siliang.chang@gmail.com>
2020-02-07 22:31:34 -0700

    s10: injecting values from properties

--- spring-demo-annotations/src/com/siliangchang/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/siliangchang/springdemo/SwimJavaConfigDemoApp.java
@@ -10,11 +10,16 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = 
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		// call dependency method on the bean
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call new swim coach methods
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
+
 		// close the context
 		context.close();
 	}
