a79df652e1bcb38934f75dc0ac0cd4e401da9815
Siliang Chang <siliang.chang@gmail.com>
2020-02-09 15:30:23 -0700

    s12: using model to manipulate data

--- spring-mvc-demo/WebContent/WEB-INF/view/helloworld.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld.jsp
@@ -5,6 +5,11 @@ Hello World of Spring!
 <br><br>
 
 Student name: ${param.studentName} 
+
+<br><br>
+
+The message: ${message}
+
 </body>
 
 </html>
--- spring-mvc-demo/src/com/siliangchang/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/siliangchang/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.siliangchang.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +18,25 @@ public class HelloWorldController {
 	// need a controller method to process the HTML form
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	// add a controller method to read form data and
+	// add data to the model
+	@RequestMapping("/processFormVersion2")
+	public String letsShout(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
 		return "helloworld";
 	}
 }
